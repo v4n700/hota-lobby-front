10f2b1ad1a87a0f9666a0dc87ba63fb29a7518e3
v4n700 <kiev6225@gmail.com>
2021-05-31 21:02:25 +0300

    Angular 10 -> 12

--- src/app/profile-page/profile-dashboard/profile-dashboard-chart/profile-dashboard-chart.component.spec.ts
+++ src/app/profile-page/profile-dashboard/profile-dashboard-chart/profile-dashboard-chart.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProfileDashboardChartComponent } from './profile-dashboard-chart.component';
 
@@ -6,7 +6,7 @@ describe('ProfileDashboardChartComponent', () => {
   let component: ProfileDashboardChartComponent;
   let fixture: ComponentFixture<ProfileDashboardChartComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProfileDashboardChartComponent ]
     })
